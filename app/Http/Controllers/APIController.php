31c4dfa9eef99209de3158dad5f732741f2c7585
JoelAmores95 <joel.amores95@gmail.com>
2023-02-27 18:02:52 +0100

    Muestra Comarcas

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -3,8 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
 
 class APIController extends Controller
 {
-    //
+
+    function guardarComarcasAPI()
+    {
+        $jsonApi = HTTP::get('https://api.idescat.cat/emex/v1/nodes.json?tipus=com&lang=es');
+        $jsonDecoded = json_decode($jsonApi);
+
+        // Guardo en array
+        foreach ($jsonDecoded->fitxes->v as $comarca) {
+            $comarcasGuardadas[] = $comarca;
+            // $comarcasGuardadas[] = $comarca->content;
+        }
+        // return view('main', compact('comarcasGuardadas'));
+        return $comarcasGuardadas;
+    }
+
+    function mostrarComarcasAPI()
+    {
+        $jsonApi = HTTP::get('https://api.idescat.cat/emex/v1/nodes.json?tipus=com&lang=es');
+        $jsonDecoded = json_decode($jsonApi);
+
+        // Guardo en array
+        foreach ($jsonDecoded->fitxes->v as $comarca) {
+            // $comarcasGuardadas[] = $comarca;
+            $comarcasGuardadas[] = $comarca->content;
+        }
+        return view('main', compact('comarcasGuardadas'));
+    }
+
+    function guardarMunicipiosAPI()
+    {
+        $jsonApi = HTTP::get('https://analisi.transparenciacatalunya.cat/resource/9aju-tpwc.json');
+        $jsonDecoded = json_decode($jsonApi);
+
+        // Guardo en array
+        foreach ($jsonDecoded as $municipio) {
+            $municipiosGuardados[] = $municipio;
+            // $municipiosGuardados[] = $municipio->nom;
+        }
+        // return view('main', compact('municipiosGuardados'));
+        return $municipiosGuardados;
+    }
+
+    // function descripcionImagenApi($municipio){
+    //     $respuestaApi = HTTP::get('https://en.wikipedia.org/w/api.php?action=query&formatversion=2&prop=pageimages%7Cpageterms&titles='.$municipio.'&pilimit=3&piprop=thumbnail&wbptterms=description&&format=json');
+    //     $jsonDecoded = json_decode($respuestaApi);
+
+    //     foreach ($jsonDecoded as $municipio){
+    //         $fotos = $municipio->query->pages->title;
+    //     }
+    //     return view('main', compact('fotos'));
+    // }
 }
---
+++ resources/views/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>API test</title>
+
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    </head>
+    <body>
+        <div class="container my-5">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                @foreach($comarcasGuardadas as $comarca)
+                <div class="col">
+                    <div class="card">
+                    
+                    <div class="card-body">
+                        <p class="card-text">{{$comarca}}</p>
+                    </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </body>
+</html>
